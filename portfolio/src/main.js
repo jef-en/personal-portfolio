b8ca2ee5e4f8fe5aa5327296e597b35e47efc072
Jefen De Villa <jefendevilla27@gmail.com>
2024-11-12 17:28:24 +0800

    Added Animations

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -4,6 +4,7 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/works.css";
 import "../styles/components/mobile-nav.css";
+import "../styles/components/footer.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
