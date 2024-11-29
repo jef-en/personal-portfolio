33678a2b7dde9d10e2332ba6bfffe439149a1eeb
Jefen De Villa <jefendevilla27@gmail.com>
2024-11-29 18:26:09 +0800

    Finish the About Section

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -6,6 +6,7 @@ import "../styles/components/works.css";
 import "../styles/components/mobile-nav.css";
 import "../styles/components/footer.css";
 import "../styles/components/about.css";
+import "../styles/components/certifications.css";
 import "../styles/utils.css";
 
 import mobileNav from "./utils/mobile-nav";
